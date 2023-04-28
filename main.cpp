9804b63433bf930e5b685741fa463cb7cbc4ce8d
julian343434 <julher2000@hotmail.es>
2023-04-28 12:14:15 -0400

    se ha creado el main.cpp y se compilo el programa primera version

--- main.cpp
+++ main.cpp
@@ -1,42 +1,121 @@
-#include "list.cpp"
+#include <iostream>
+#include <queue>
+#include <random>
+#include <thread>
+#include <chrono>
+#include <ncurses.h>
 
-using namespace std;
+// Clase Vehiculo
+class Vehiculo {
+public:
+    int categoria; // Categoría del vehículo
 
-int main(){
-    List<string> list;
-    list.insert(0, "Victor");
-    list.insert(1, "Amanda");
-    list.insert(2, "Ali");
-    list.insert(3, "Primera");
-    list.insert(4, "Jose");
-
-    cout << "Imprimir: " << endl;
-    list.print();
-
-    cout << "Obteniendo algunos nodos" << endl;
-    cout << list.get(0) << endl;
-    cout << list.get(2) << endl;
-    
-    cout << "Borrando algunos nodos" << endl;
-    list.erase(0);
-    list.erase(3);
-    list.print();
-
-    cout << "Evaluando el tamanio" << endl;
-    cout << "Tamanio: "<< list.size()<<endl;
-    cout << "se busca un valor en la lista "<<endl;
-    list.search("nuevo valor");
-
-    cout << "Actualaiza una posicion de la lista"<<endl;
-    list.update(0,"pollo frito");    
-    cout<<list.get(2)<<endl;
-    
-    cout<<"Obtienen todos los elementos de la lista en un array"<<endl;
-    vector<string> allElements = list.getAll();
-    cout<<"imprime todos los elementos del vector asignado desde la lista"<<endl;
-    for (int i = 0; i < allElements.size(); i++) {
-        cout << allElements[i] << " "<<endl;
+    // Constructor
+    Vehiculo(int cat) {
+        categoria = cat;
     }
 
-    return 0;
+    // Método para obtener la tarifa del peaje
+    float obtenerTarifa() {
+        float tarifa;
+        switch (categoria) {
+            case 1:
+                tarifa = 5.0;
+                break;
+            case 2:
+                tarifa = 10.0;
+                break;
+            case 3:
+                tarifa = 2.0;
+                break;
+            default:
+                tarifa = 0.0;
+        }
+        return tarifa;
+    }
+};
+
+// Función para generar un vehículo aleatorio
+Vehiculo generarVehiculoAleatorio() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(1, 3);
+
+    int categoria = distr(gen);
+    Vehiculo vehiculo(categoria);
+
+    return vehiculo;
+}
+
+int main() {
+    // Inicializar ncurses
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+
+    // Crear colas
+    std::queue<Vehiculo> cola1;
+    std::queue<Vehiculo> cola2;
+
+    // Generar vehículos aleatorios y agregar a colas
+    for (int i = 0; i < 10; i++) {
+        Vehiculo vehiculo = generarVehiculoAleatorio();
+        if (vehiculo.categoria == 1) {
+            cola1.push(vehiculo);
+        } else {
+            cola2.push(vehiculo);
+        }
+    }
+
+    // Loop principal
+    while (true) {
+        // Limpiar pantalla
+        clear();
+
+        // Mostrar colas
+        mvprintw(0, 0, "Cola 1:");
+        int i = 1;
+        std::queue<Vehiculo> cola1_temp = cola1;
+        while (!cola1_temp.empty()) {
+            Vehiculo vehiculo = cola1_temp.front();
+            mvprintw(i, 0, "Categoria %d", vehiculo.categoria);
+            cola1_temp.pop();
+            i++;
+        }
+        mvprintw(i+1, 0, "---------------------");
+        mvprintw(i+2, 0, "Cola 2:");
+        int j = i+3;
+        std::queue<Vehiculo> cola2_temp = cola2;
+        while (!cola2_temp.empty()) {
+            Vehiculo vehiculo = cola2_temp.front();
+            mvprintw(j, 0, "Categoria %d", vehiculo.categoria);
+            cola2_temp.pop();
+            j++;
+        }
+
+        // Actualizar pantalla
+        refresh();
+
+        // Generar vehículo aleatorio y agregar a cola
+        Vehiculo vehiculo = generarVehiculoAleatorio();
+        if (vehiculo.categoria == 1) {
+            cola1.push(vehiculo);
+        } else {
+            cola2.push(vehiculo);
+        }
+
+        // Esperar un tiempo
+    // Esperar un tiempo aleatorio entre 1 y 10 segundos
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(1, 10);
+    int tiempo = distr(gen);
+    std::this_thread::sleep_for(std::chrono::seconds(tiempo));
+}
+
+// Finalizar ncurses
+endwin();
+
+return 0;
 }
